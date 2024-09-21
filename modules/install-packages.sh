53985e4a0d7fdaaf6090d169622a4ac42a1b3b00
lu-papagni <papagniluca4@gmail.com>
2024-09-21 17:35:32 +0200

    Finito modulo installazione pacchetti

--- modules/install-packages.sh
+++ modules/install-packages.sh
@@ -1,105 +1,167 @@
 #!/usr/bin/env bash
 
-# ██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗███████╗
-# ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝██╔════╝
-# ██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  ███████╗
-# ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  ╚════██║
-# ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║
-# ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝
-
 [[ -v __DEFINE_PACKAGEINST ]] && return
 readonly __DEFINE_PACKAGEINST
 
-function InstallPackages() {
-  local -r src_dir="packagelist"
-  local sources=()
+SOURCES_SRC_DIR="packagelist"
 
-    # Controllo se sono stati forniti i parametri
-    if [[ -z $sources ]]; then
-      echo "Installazione pacchetti: non sono state forniti i parametri."
-      return 1
-    fi
+# Effettua il setup del package manager
+function Setup-PackageManager() {
+  local -r manager="$1"
 
-    # Controllo se esiste la directory
-    if [[ ! -d "$src_dir" ]]; then
-      echo "Era prevista una directory \`$src_dir\` nella directory di lavoro, ma non è stata trovata."
-      return 1
-    fi
-
-    # Parsing del file
-    for source in ${sources[@]}; do
-      local pgk_manager=""
-      local install_cmd=""
-      local packages=()
-      local privilege=""
-      local run_as=""
-      local skip_confirm_cmd=""
-
-      echo "Lettura lista dei pacchetti: \`$source\`"
-
-      # Controllo se il file è leggibile
-      if [[ ! -r "$src_dir/$source.txt" ]]; then
-        echo "La lista \`$source\` non è leggibile. Proseguo..."
-        continue
+  case "$manager" in
+    'yay')
+      if [[ "$(whoami)" != "root" ]]; then
+        yay -Syu
+      else if [[ -n "$SUDO_USER" ]]; then
+        sudo -u "$SUDO_USER" yay -Syu
+      else
+        echo "L'amministratore non può effettuare il setup di \`yay\`"
       fi
+      fi
+      ;;
+    'flatpak')
+      flatpak remote-add --if-not-exixts 'flathub' 'https://dl.flathub.org/repo/flathub.flatpakrepo'
+      ;;
+    'dnf')
+      echo "ATTENZIONE: nessun setup specifico per \`$manager\`"
+      ;;
+    'apt' | 'apt-get')
+      echo "ATTENZIONE: nessun setup specifico per \`$manager\`"
+      ;;
+  esac
+}
 
-      # Leggo il package manager da usare alla prima riga del file
-      pkg_manager="$(head -n 1 "$src_dir/$source.txt" | sed s/\!//)"
+# Ottiene i nomi delle fonti per un package manager
+function Get-PackageSources() {
+  local -r manager="$1"
 
-      # Salvo ogni pacchetto da installare, uno per riga
-      for line in $(tail -n +2 "$src_dir/$source.txt"); do
-        packages+=("$line")
-      done
+  if [[ -n "$manager" ]]; then
+    echo -n "$(find "$SOURCES_SRC_DIR/$manager" -mindepth 1 -type f -name '*.txt')"
+  else
+    return 1
+  fi
+}
 
-      # Imposto i comandi da eseguire in base al package manager
-      case "$pkg_manager" in
-        yay | pacman)
-          skip_confirm_cmd="--noconfirm"
-          ;;
-        pacman)
-          [[ "$(whoami)" != "root" ]] && privilege="sudo"
+# Rimuove le fonti indesiderate per indice
+function Remove-UnwantedSources() {
+  local remove_indices
+  local sources=("$@")
+
+  # Stampo lista
+  for i in ${!sources[@]}; do
+    local source_name="$(basename "${sources[$i]}")"
+    source_name="${source_name/%\.*/}"
+
+    # Stampo su stderr
+    echo "$((i+1))) $source_name" >&2
+  done
+
+  read -p "Fonti da ignorare (separate da spazi, INVIO per annullare): " -a remove_indices
+  
+  # Rimuovo gli elementi
+  for k in "$remove_indices"; do
+    [[ "$k" =~ [[:digit:]] ]] && unset sources[$((k-1))]
+  done
+
+  echo -n "${sources[@]}"
+}
+
+# Ottiene il comando che determina i privilegi del package manager
+function Get-PrivilegePrefix() {
+  local privilege
+  local -r run_as="$1"
+  local -r username="$([[ -n "$SUDO_USER" ]] && echo "$SUDO_USER" || whoami)"
+
+  case "$run_as" in
+    'root')
+      [[ "$username" = "root" ]] && privilege="" || privilege="sudo"
+      ;;
+    'user')
+      if [[ "$username" != "root" ]]; then
+        privilege="sudo -u $username"
+      else
+        echo "Impossibile eseguire come root!" >&2
+        return 1
+      fi
+      ;;
+    *)
+      echo "Il valore di \`run_as\` non può essere \`$run_as\`" >&2
+      return 2
+      ;;
+  esac
+
+  echo -n "$privilege"
+}
+
+# Logica principale installazione
+function Install-Packages() {
+  # Controllo se esiste la directory
+  if [[ ! -d "$SOURCES_SRC_DIR" ]]; then
+    echo "Era prevista una directory \`$SOURCES_SRC_DIR\` nella directory di lavoro, ma non è stata trovata."
+    return 1
+  fi
+
+  local -r managers=("$(find "$SOURCES_SRC_DIR" -mindepth 1 -maxdepth 1 -type d | xargs -I{} basename {})")
+
+  # Parsing del file
+  for manager in ${managers[@]}; do
+    if command -v "$manager" &> /dev/null; then
+      local install_cmd
+      local run_as
+      local skip_confirm_cmd
+
+      case "$manager" in
+        'yay')
           install_cmd="-S --needed"
-          ;;
-        yay)
-          run_as="$SUDO_USER"
-          install_cmd="-S --needed --clean"
-          ;;
-        flatpak)
-          install_cmd="install"
+          run_as="user"
           skip_confirm_cmd="--assumeyes"
           ;;
-        dnf | apt | 'apt-get')
-          [[ "$(whoami)" != "root" ]] && privilege="sudo"
+        'dnf')
           install_cmd="install"
+          run_as="root"
+          skip_confirm_cmd="-y"
+          ;;
+        'apt' | 'apt-get')
+          install_cmd="install"
+          run_as="root"
+          skip_confirm_cmd="-y"
+          ;;
+        'flatpak')
+          install_cmd="install"
+          run_as="user"
           skip_confirm_cmd="-y"
           ;;
         *)
-          Log --error "$(Highlight "$pkg_manager") non è supportato."
-          return 1
+          echo "Il package manager non è supportato."
           ;;
       esac
 
-      # Controllo che il package manager sia installato
-      if ! command -v "$pkg_manager" &> /dev/null; then
-        echo "Il package manager \`$pkg_manager\` non è presente sul sistema."
-        return 1
-      fi
+      # Ottengo le fonti
+      local sources=("$(Get-PackageSources "$manager")")
 
-      echo "Installazione pacchetti, fonte \`$source\`"
+      echo "Trovate ${#sources[@]} fonti per \`$manager\`:"
 
-      # Comando per installare i pacchetti
-      [[ -z $run_as ]] && privilege="$privilege -u $run_as"
-      sh -c "$privilege $pkg_manager $install_cmd $skip_confirm_cmd ${packages[@]}"
+      # Rimuovo gli elementi
+      sources="$(Remove-UnwantedSources "$sources")"
 
-    done # Fine parsing
+      # Installo i pacchetti
+      for source in ${sources[@]}; do
+        local packages
 
-    # TODO: Spostare la config di KDE nel suo modulo
+        if [[ ! -r "$source" ]]; then
+          echo "Fonte \`${source}\` illeggibile."
+          continue
+        fi
 
-   # Se KDE deve essere configurato
-   if [[ -n "$SETUP_KONSAVE_PROFILE" && -r "$HOME/.dotfiles/konsave/profiles/$SETUP_KONSAVE_PROFILE" ]]; then
-     Log "Caricamento profilo $(Highlight "$SETUP_KONSAVE_PROFILE")..."
-     AssertExecutable "konsave" && konsave -a "$HOME/konsave/profiles/$SETUP_KONSAVE_PROFILE"
-   fi
+        # Leggo i pacchetti da file
+        mapfile -t packages < "$source"
 
-   Log "... fine installazione pacchetti."
+        echo "Installo i pacchetti dalla fonte \`$source\`..."
+        sh -c "$(Get-PrivilegePrefix "$run_as") $manager $install_cmd ${packages[*]} $skip_confirm_cmd"
+      done
+    else
+      echo "Package manager \`$manager\` non trovato: salto la fonte."
+    fi
+  done
  }
