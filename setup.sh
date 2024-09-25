de63a826b677ce67e1b4d2cdd2637b422c1d13e5
lu-papagni <papagniluca4@gmail.com>
2024-09-25 22:49:38 +0200

    Reso piu robusto lo script principale

--- setup.sh
+++ setup.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
-source ./modules/*.sh
+# Importa tutti i moduli
+for f in modules/*.sh; do source $f; done
 
 DOTS_DIR="$HOME/.dotfiles"
 DOTS_REPO='https://github.com/lu-papagni/dots.git' 
 SOURCES_SRC_DIR="packagelist"
 
+if [[ ! -d "$DOTS_DIR" ]]; then
+  mkdir -p "$DOTS_DIR"
+  git clone --recurse-submodules "$DOTS_REPO" "$DOTS_DIR"
+  
+  if [[ $? -ne 0 ]]; then
+    echo "Clonazione di \`$DOTS_REPO\` fallita!"
+    exit 1
+  fi
+fi
+
 Setup-System
 Setup-Distribution
 Install-Packages
 Setup-DesktopEnvironment
-
-mkdir -p "$DOTS_DIR"
-git clone --recurse-submodules "$DOTS_REPO" "$DOTS_DIR"
-
-if [[ $? -ne 0 ]]; then
-  echo "Clonazione di \`$DOTS_REPO\` fallita!"
-  exit 1
-fi
-
 Link-ConfigFiles "conf/link-ignore.txt"
 Setup-Systemd
 Setup-Apps
 
 # Cambio shell
-chsh -s "$(command -v 'zsh')" "${SUDO_USER:-$(whoami)}"
+if [[ "$SHELL" != *zsh ]]; then
+  echo "Cambio shell di default"
+  chsh -s "$(command -v 'zsh')" "${SUDO_USER:-$(whoami)}"
+fi
 
 echo "Fatto!"
