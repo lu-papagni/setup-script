83f50afb9decb7438a4bfd56464afa0dd4573601
lu-papagni <papagniluca4@gmail.com>
2024-09-25 20:10:09 +0200

    Corretti piccoli errori in `link-config`

--- modules/link-config.sh
+++ modules/link-config.sh
@@ -2,18 +2,18 @@
 
 function Link-ConfigFiles() {
   local -r conf_dir="${DOTS_DIR:-"~/.dotfiles"}"
-  local -r ignorefile='link-ignore.txt'
-  local file_except="$(mktemp)"
-  local dir_except="$(mktemp)"
+  local -r ignorefile="${1:-"link-ignore.txt"}"
+  local -r file_except="$(mktemp)"
+  local -r dir_except="$(mktemp)"
 
-  if [[ -r "$ignorefile" ]]; then
+  if [[ -f "$ignorefile" ]]; then
     local line
 
     while read -r line; do
       [[ -z "$line" ]] && continue
 
       if [[ "$line" == */ ]]; then
-        echo "${line/%\//}" >> "$dir_except"
+        echo "${line%\/}" >> "$dir_except"
       else
         echo "$line" >> "$file_except"
       fi
@@ -21,19 +21,31 @@ function Link-ConfigFiles() {
 
     unset line
   else
-    echo "Impossibile trovare il file delle eccezioni: uso quelle di default." >&2
+    echo "Impossibile trovare il file delle eccezioni \`$ignorefile\`" >&2
+    echo "Uso le impostazioni di default." >&2
 
-    echo 'DOCS' >> "$dir_except" 
-    echo '.git' >> "$dir_except" 
+    # Directory
     echo 'setup' >> "$dir_except" 
+    echo '.git*' >> "$dir_except"
 
+    # File
     echo '.git*' >> "$file_except"
     echo '*.md' >> "$file_except"
-    echo 'user_*.*' >> "$file_except"
-    echo '.zshrc.ohmyzsh' >> "$file_except"
+    echo '.*.ohmyzsh' >> "$file_except"
   fi
 
-  # Trovo e collego i file
-  find "$conf_dir" -mindepth 1 -maxdepth 1 -type d | grep -vGf "$dir_except" | xargs -I{} ln -s "~/.config"
-  find "$conf_dir" -mindepth 1 -maxdepth 1 -type f | grep -vGf "$file_except" | xargs -I{} ln -s "~"
+  # Link directory in /home/utente/.config
+  echo "Directory: \`$conf_dir\` -> \`$HOME/.config\`" >&2
+
+  mkdir -p "$HOME/.config"
+  find "$conf_dir" -mindepth 1 -maxdepth 1 -type d |
+    grep -vGf "$dir_except" |
+    xargs -I{} ln -s {} "$HOME/.config"
+
+  # Link file liberi in /home/utente
+  echo "File: \`$conf_dir\` -> \`$HOME\`" >&2 
+
+  find "$conf_dir" -mindepth 1 -maxdepth 1 -type f |
+    grep -vGf "$file_except" |
+    xargs -I{} ln -s {} "$HOME"
 }
