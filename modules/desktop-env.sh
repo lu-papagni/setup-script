5f642039629481963c52a9833200417bcf6b9700
lu-papagni <papagniluca4@gmail.com>
2024-09-25 22:48:18 +0200

    Coretta importazione impostazioni di KDE

--- modules/desktop-env.sh
+++ modules/desktop-env.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
-function Import-PlasmaSettings() {
-  local -r profile="$1"
-
-  if [[ -r "$profile_path" ]]; then
-    echo "Caricamento profilo \`$profile\`"
-    konsave -a "$profile"
-  fi
-}
-
 function Setup-DesktopEnvironment() {
   if [[ -v KDE_SESSION_VERSION ]]; then
     local plasma_profile
 
     echo "KDE Plasma (ver. $KDE_SESSION_VERSION) rilevato."
-    read -p "Nome del profilo da caricare" plasma_profile
 
-    Import-PlasmaSettings "$plasma_profile"
+    read -p "Nome del profilo KDE da caricare: " plasma_profile
+    konsave -a "$plasma_profile"
   fi
 }
