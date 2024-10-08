ea500c65089f9a3a13029fd02ae83d09d68a93e8
lu-papagni <papagniluca4@gmail.com>
2024-10-08 12:42:45 +0200

    Aggiunta opzione per cambiare hostname

--- modules/config-system.sh
+++ modules/config-system.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
+function Set-Hostname() {
+  local static
+  local pretty
+
+  echo "Hostname breve corrente: '$(hostnamectl --static)'"
+  echo "Hostname dettagliato corrente: '$(hostnamectl --pretty)'"
+  echo "Inserire nuovi hostname (lascia vuoto per ignorare)"
+
+  read -p "Breve: " static 
+  read -p "Dettagliato: " pretty 
+
+  [[ -n "$static" ]] && hostnamectl --static hostname "$static"
+  [[ -n "$pretty" ]] && hostnamectl --pretty hostname "$pretty"
+}
+
 function Setup-System() {
+  Set-Hostname
 }
