c136946275697627e915b0f63438a54f1b86546f
lu-papagni <papagniluca4@gmail.com>
2025-03-11 19:15:38 +0100

    feat(utils.sh): aggiunta funzione per controllo della distribuzione

--- plugin/utils.sh
+++ plugin/utils.sh
@@ -5,3 +5,7 @@ __import_utils=1
 alias perror="printf '\e[1;31mERRORE: %s\e[0m\n'"
 alias pwarn="printf '\e[1;33mATTENZIONE: %s\e[0m\n'"
 alias pinfo="printf '\e[1;32mINFO: %s\e[0m\n'"
+
+function get_distro() {
+  echo "$(grep 'ID=' /etc/os-release | sed 's/ID=//')"
+}
